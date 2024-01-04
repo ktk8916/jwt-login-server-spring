424430b7e2d8339a11c75ad64195817630334104
ktk8916 </xorud9933>
2024-01-04 15:09:45 +0900

    board 예외 메세지 추가

--- src/main/java/com/nitsoft/login/global/exception/ExceptionType.java
+++ src/main/java/com/nitsoft/login/global/exception/ExceptionType.java
@@ -7,8 +7,16 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 @Getter
 public enum ExceptionType {
+
+    // auth
     MEMBER_NOT_FOUND(HttpStatus.UNAUTHORIZED, "member not found"),
-    USERNAME_ALREADY_REGISTERED(HttpStatus.BAD_REQUEST, "username already registered")
+    EMAIL_ALREADY_REGISTERED(HttpStatus.BAD_REQUEST, "email already registered"),
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "invalid refresh token"),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "invalid password"),
+
+    //board
+    UNAUTHORIZED_CONTENT_OWNER(HttpStatus.FORBIDDEN, "unauthorized content owner"),
+    CONTENT_NOT_FOUND(HttpStatus.NOT_FOUND, "content not found"),
 
     ;
 
