70ef59fd3c5953ad8761a848e04f11c1a01b498e
ktk8916 <ktk8916@naver.com>
2024-01-09 17:22:42 +0900

    닉네임 변경 validation 추가

--- src/main/java/com/nitsoft/login/member/controller/MemberController.java
+++ src/main/java/com/nitsoft/login/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.nitsoft.login.member.controller;
 
 import com.nitsoft.login.global.jwt.TokenInfo;
 import com.nitsoft.login.member.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -18,7 +19,7 @@ public class MemberController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void editMember(
             @AuthenticationPrincipal TokenInfo tokenInfo,
-            @RequestBody MemberRequest request
+            @RequestBody @Valid MemberRequest request
     ){
         memberService.editMember(tokenInfo.getId(), request);
     }
--- src/main/java/com/nitsoft/login/member/controller/MemberRequest.java
+++ src/main/java/com/nitsoft/login/member/controller/MemberRequest.java
@@ -1,9 +1,13 @@
 package com.nitsoft.login.member.controller;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
+import org.hibernate.validator.constraints.Length;
 
 @Builder
 public record MemberRequest(
+        @NotBlank(message = "nickname is required")
+        @Length(max = 20, message = "nickname must be less than 20 characters")
         String nickname
 ) {
 }
